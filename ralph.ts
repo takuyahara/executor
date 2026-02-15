13053e8a3f48072743bf75f2679d679840f0a987
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2026-02-14 23:52:20 -0800

    ralph

--- ralph.ts
+++ ralph.ts
@@ -1,20 +1,30 @@
 import { $ } from "bun"
 import { readFileSync } from "node:fs"
-const coreRefactorGoal = readFileSync("executor/convex-core-refactor-code-plan", "utf-8")
+const coreRefactorGoal = readFileSync("./executor/convex-core-refactor-code-plan.md", "utf-8")
+const betterResultMigrationGoal = readFileSync(
+    "./executor/better-result-migration.md",
+    "utf-8"
+)
 
 const STOP_TOKEN = '<I HAVE COMPLETED THE TASK>'
 
 const makePrompt = (goal: string) => `Continue working until you believe the task is complete. As a reminder, the goal is: ${goal}. The above goal was copy pasted in, resume from where you left off. Output ${STOP_TOKEN} when you have completed the task.`
 
 
-async function run(goal: string) {
+async function run(goal: string, model: 'openai/gpt-5.3-codex' | 'anthropic/claude-opus-4-6') {
     const prompt = makePrompt(goal)
     let ralph = ''
+    let firstRun = true
     while (!ralph.includes(STOP_TOKEN)) {
-        ralph = await $`opencode run --attach http://100.81.219.45:39821 --continue ${prompt}`.text()
+        const command = firstRun
+            ? $`opencode run --attach http://100.81.219.45:39821 --model ${model} ${prompt}`
+            : $`opencode run --attach http://100.81.219.45:39821 --model ${model} --continue ${prompt}`
+
+        ralph = await command.text()
+        firstRun = false
     }
-    return ralph
+    await $`opencode --run Make a git commit of your changes, do not push`
 }
 
-await run(coreRefactorGoal)
-
+await run(coreRefactorGoal, 'openai/gpt-5.3-codex')
+await run(betterResultMigrationGoal, 'anthropic/claude-opus-4-6')
